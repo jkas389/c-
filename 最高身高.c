3cf4ba88dd9fa99796c7937924a267faffd9c848
textapu <147218289+jkas389@users.noreply.github.com>
2023-11-16 22:09:03 +0800

    Update 最高身高.c

--- 最高身高.c
+++ 最高身高.c
@@ -2,9 +2,9 @@
 int main()
 {
 	int n,m;
-	int a[10][10];
 	scanf("%d %d",&n,&m);
-	int i,j;
+	int a[n][m];
+	int i,j,x,y;
 	int max = 0;
 	for(i=0;i<n;i++)
 	{
@@ -13,10 +13,12 @@ int main()
 			scanf("%d",&a[i][j]);
 			if(a[i][j]>max)
 			{
+				x=n+1;
+				y=j+1;
 				max = a[i][j];
 			}
 		}
 	}
-	printf("%d %d",i,j);
+	printf("%d %d",x,y);
 	return 0;
 }
